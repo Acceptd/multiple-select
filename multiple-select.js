b0f440315dea065307e9cd6573a6a17e3755f077
David Ensinger <densinger@getacceptd.com>
2017-07-19 12:46:39 -0400

    Add empty .control-indicator span for custom checkbox styling

--- multiple-select.js
+++ multiple-select.js
@@ -211,6 +211,7 @@
                     '<li class="ms-select-all">',
                     '<label>',
                     sprintf('<input type="checkbox" %s /> ', this.selectAllName),
+                    '<span class="control-indicator"></span>',
                     this.options.selectAllDelimiter[0],
                     this.options.selectAllText,
                     this.options.selectAllDelimiter[1],
@@ -274,6 +275,7 @@
                         selected ? ' checked="checked"' : '',
                         disabled ? ' disabled="disabled"' : '',
                         sprintf(' data-group="%s"', group)),
+                    '<span class="control-indicator"></span>',
                     sprintf('<span>%s</span>', text),
                     '</label>',
                     '</li>'
@@ -294,6 +296,7 @@
                     this.options.hideOptgroupCheckboxes || this.options.single ? '' :
                         sprintf('<input type="checkbox" %s %s>',
                         this.selectGroupName, disabled ? 'disabled="disabled"' : ''),
+                        '<span class="control-indicator"></span>',
                     label,
                     '</label>',
                     '</li>'
